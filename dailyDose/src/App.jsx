8e2b453d8c34cd172deaa5bc886210cae45b55a0
Abhinav bhar <abhinavbhar2004@gmail.com>
2024-02-27 00:52:22 +0530

    Scrapper added

--- dailyDose/src/App.jsx
+++ dailyDose/src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css'
 import Navbar from './Components/Navbar'
-
-function App() {
-
-  return (
+import NewsMain from './Components/NewsMain'
+import Ipl from './Components/Ipl';
+import Wwe from './Components/wwe';
+import Politics from './Components/Politics'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Finance from './Components/Finance';
+function Home(){
+  return(
     <>
-    <Navbar></Navbar>
-      
+    <NewsMain></NewsMain>
     </>
   )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar></Navbar>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ipl" element={<Ipl/>} />
+      <Route path='/wwe' element={<Wwe/>} />
+      <Route path='/politics' element={<Politics/>} />
+      <Route path='/finance' element={<Finance/>} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
--- dailyDose/src/Components/Navbar.jsx
+++ dailyDose/src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand" href="/">
                     <img src={logo} style={{ maxHeight: '50px' }} alt="company logo" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,22 +15,19 @@ function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">IPL</a>
+                            <a className="nav-link" href="ipl">IPL</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">WWE</a>
+                            <a className="nav-link" href="wwe">WWE</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Finance</a>
+                            <a className="nav-link" href="finance">Finance</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Politics</a>
+                            <a className="nav-link" href="politics">Politics</a>
                         </li>
                     </ul>
-                    <div className="search-bar flex">
-                        <input id="search-text" type="text" className="news-input" placeholder="e.g. Science" />
-                        <button id="search-button" className="search-button">Search</button>
-                    </div>
+                    
                 </div>
             </div>
         </nav>
